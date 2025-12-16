1bfe7559f164ab4481adc5c17676443fbfb25955
Muhammad Ghufron Khaical Alfaris <alfaris.mgk@gmail.com>
2025-12-16 11:11:20 +0700

    feat: Add API key authentication middleware and protect employee routes.

--- routes/employees.js
+++ routes/employees.js
@@ -1,11 +1,12 @@
 const express = require("express");
 const router = express.Router();
 const controller = require("../controllers/employeeController");
+const authApiKey = require("../middlewares/apiKey");
 
-router.get("/", controller.getAllEmployees);
-router.get("/:id", controller.getEmployeeById);
-router.post("/", controller.createEmployee);
-router.put("/:id", controller.updateEmployee);
-router.delete("/:id", controller.deleteEmployee);
+router.get("/", authApiKey, controller.getAllEmployees);
+router.get("/:id", authApiKey, controller.getEmployeeById);
+router.post("/", authApiKey, controller.createEmployee);
+router.put("/:id", authApiKey, controller.updateEmployee);
+router.delete("/:id", authApiKey, controller.deleteEmployee);
 
 module.exports = router;
