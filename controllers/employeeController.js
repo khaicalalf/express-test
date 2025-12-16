68e29ee67410669f628e94ebcbdd0796d95383a3
Muhammad Ghufron Khaical Alfaris <alfaris.mgk@gmail.com>
2025-12-16 10:24:14 +0700

    feat: Add employee CRUD controller with pagination and search capabilities.

--- controllers/employeeController.js
+++ controllers/employeeController.js
@@ -3,9 +3,53 @@ const { poolPromise, sql } = require("../db");
 // GET all employees
 exports.getAllEmployees = async (req, res) => {
   try {
+    let { page = 1, limit = 10, search = "" } = req.query;
+
+    page = parseInt(page);
+    limit = parseInt(limit);
+
+    if (page < 1 || limit < 1) {
+      return res.status(400).json({
+        message: "Page dan limit harus lebih dari 0",
+      });
+    }
+
+    const offset = (page - 1) * limit;
+
     const pool = await poolPromise;
-    const result = await pool.request().query("SELECT * FROM Employees");
-    res.json(result.recordset);
+
+    // TOTAL DATA
+    const countResult = await pool
+      .request()
+      .input("search", sql.VarChar, `%${search}%`).query(`
+        SELECT COUNT(*) AS total
+        FROM Employees
+        WHERE name LIKE @search
+      `);
+
+    const totalData = countResult.recordset[0].total;
+    const totalPages = Math.ceil(totalData / limit);
+
+    // DATA PAGINATION
+    const dataResult = await pool
+      .request()
+      .input("search", sql.VarChar, `%${search}%`)
+      .input("offset", sql.Int, offset)
+      .input("limit", sql.Int, limit).query(`
+        SELECT *
+        FROM Employees
+        WHERE name LIKE @search
+        ORDER BY id
+        OFFSET @offset ROWS FETCH NEXT @limit ROWS ONLY
+      `);
+
+    res.json({
+      page,
+      limit,
+      totalData,
+      totalPages,
+      data: dataResult.recordset,
+    });
   } catch (err) {
     res.status(500).json({ error: err.message });
   }
@@ -30,12 +74,24 @@ exports.getEmployeeById = async (req, res) => {
 exports.createEmployee = async (req, res) => {
   const { name, position, salary } = req.body;
 
+  if (!name || name.trim() === "") {
+    return res.status(400).json({
+      message: "Name tidak boleh kosong",
+    });
+  }
+
+  if (!salary || salary <= 0) {
+    return res.status(400).json({
+      message: "Salary harus lebih dari 0",
+    });
+  }
+
   try {
     const pool = await poolPromise;
     await pool
       .request()
       .input("name", sql.VarChar, name)
-      .input("position", sql.VarChar, position)
+      .input("position", sql.VarChar, position || "")
       .input("salary", sql.Int, salary)
       .query(
         "INSERT INTO Employees (name, position, salary) VALUES (@name, @position, @salary)"
